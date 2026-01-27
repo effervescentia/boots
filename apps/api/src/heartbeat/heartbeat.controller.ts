23f83333da6825577d5aa4277aa3b35fc8cba419
Ben Teichman <ben@voiceflow.com>
2026-01-27 00:43:20 -0500

    feat: hearbeat data

--- apps/api/src/heartbeat/heartbeat.controller.ts
+++ apps/api/src/heartbeat/heartbeat.controller.ts
@@ -1,5 +1,6 @@
 import { AuthPlugin } from '@api/auth/auth.plugin';
 import { DatabasePlugin } from '@api/db/db.plugin';
+import { updateOne } from '@bltx/db';
 import { and, eq } from 'drizzle-orm';
 import Elysia, { NotFoundError, t } from 'elysia';
 import { CreateHeartbeatRequest } from './data/create-heartbeat.req';
@@ -8,6 +9,12 @@ import { HeartbeatDTO } from './data/heartbeat.dto';
 import { HeartbeatService } from './heartbeat.service';
 
 const HeartbeatParams = t.Object({ heartbeatID: t.String({ format: 'uuid' }) });
+
+class HeartbeatNotFoundError extends NotFoundError {
+  constructor(heartbeatID: string) {
+    super(`No Heartbeat exists with ID '${heartbeatID}'`);
+  }
+}
 
 export const HeartbeatController = new Elysia({ prefix: '/heartbeat' })
   .use(DatabasePlugin)
@@ -32,7 +39,30 @@ export const HeartbeatController = new Elysia({ prefix: '/heartbeat' })
       const heartbeat = await db().query.HeartbeatDB.findFirst({
         where: and(eq(HeartbeatDB.id, params.heartbeatID), eq(HeartbeatDB.accountID, principal.id)),
       });
-      if (!heartbeat) throw new NotFoundError(`No Heartbeat exists with ID '${params.heartbeatID}'`);
+      if (!heartbeat) throw new HeartbeatNotFoundError(params.heartbeatID);
+
+      return heartbeat;
+    },
+    {
+      authenticated: true,
+      params: HeartbeatParams,
+      response: HeartbeatDTO,
+    },
+  )
+
+  .head(
+    '/:heartbeatID',
+    async ({ db, params, principal }) => {
+      // const heartbeat = await db().query.HeartbeatDB.findFirst({
+      //   where: and(eq(HeartbeatDB.id, params.heartbeatID), eq(HeartbeatDB.accountID, principal.id)),
+      // });
+      const heartbeat = await updateOne(
+        db(),
+        HeartbeatDB,
+        and(eq(HeartbeatDB.id, params.heartbeatID), eq(HeartbeatDB.accountID, principal.id))!,
+        { updatedAt: new Date() },
+      );
+      if (!heartbeat) throw new HeartbeatNotFoundError(params.heartbeatID);
 
       return heartbeat;
     },
